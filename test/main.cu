120f08079060c17cdb29337b7bedfc42ed6ec3d4
Carl Yang <ctcyang@ucdavis.edu>
2014-12-21 15:14:47 -0800

    break up into many files

--- test/main.cu
+++ test/main.cu
@@ -6,29 +6,25 @@
 // -test whether float really are faster than ints
 // -distributed idea
 // -change nthread [done - doesn't work]
-
+ 
 #include <cstdlib>
+#include <ctime>
 #include <stdio.h>
+#include <iostream>
 #include <cuda_runtime_api.h>
 #include <cuda.h>
 #include <cusparse.h>
+#include <cuda_profiler_api.h>
+
+#include <cublas_v2.h>
+#include <sys/resource.h>
 #include <deque>
-
 #include <util.cuh>
-#include <bfs.cuh>
 
+#define N (100*1024)
+//#define NBLOCKS 16384
+#define NTHREADS 1024
 #define MARK_PREDECESSORS 0
-
-// Verify the result
-void verify( const int m, const int *h_bfsResult, const int *h_bfsResultCPU ){
-    if (h_bfsResultCPU != NULL) {
-        printf("Label Validity: ");
-        int error_num = CompareResults(h_bfsResult, h_bfsResultCPU, m, true);
-        if (error_num > 0) {
-            printf("%d errors occurred.\n", error_num);
-        }
-    }
-}
 
 /******************************************************************************
  * BFS Testing Routines
@@ -194,6 +190,182 @@ void csr2csc( const int m, const int edge, const float *d_csrValA, const int *d_
     cusparseDestroy(handle);
 }
 
+void axpy( float *d_spmvSwap, const float *d_csrValA, const int m ) {
+    const float alf = -1;
+    const float *alpha = &alf;
+
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+
+    cublasStatus_t status = cublasSaxpy(handle, 
+                            m, 
+                            alpha, 
+                            d_csrValA, 
+                            1, 
+                            d_spmvSwap, 
+                            1);
+
+    switch( status ) {
+        case CUBLAS_STATUS_SUCCESS:
+	    //printf("axpy completed successfully!\n");
+            break;
+        case CUBLAS_STATUS_NOT_INITIALIZED:
+	    printf("The library was not initialized.\n");
+	    break;
+        case CUBLAS_STATUS_ARCH_MISMATCH:	
+	    printf("The device does not support double-precision.\n");
+	    break;
+        case CUBLAS_STATUS_EXECUTION_FAILED:
+	    printf("The function failed to launch on the GPU.\n");
+    }
+
+    cublasDestroy(handle);
+}
+
+void spmv( const float *d_inputVector, const int edge, const int m, const float *d_csrValA, const int *d_csrRowPtrA, const int *d_csrColIndA, float *d_spmvResult ) {
+    const float alf = 1;
+    const float bet = 0;
+    const float *alpha = &alf;
+    const float *beta = &bet;
+
+    cusparseHandle_t handle;
+    cusparseCreate(&handle);
+
+    cusparseMatDescr_t descr;
+    cusparseCreateMatDescr(&descr);
+    //cusparseSetMatIndexBase(descr, CUSPARSE_INDEX_BASE_ONE);
+
+    //cusparseStatus_t status = cusparseScsrmv(handle, CUSPARSE_OPERATION_NON_TRANSPOSE, m, m, alpha, descr, d_csrValA, d_csrRowPtrA, d_csrColIndA, d_inputVector, beta, d_spmvResult);
+
+    // For CUDA 5.0+
+    cusparseStatus_t status = cusparseScsrmv(handle,                   
+                              CUSPARSE_OPERATION_NON_TRANSPOSE, 
+                              m, m, edge, 
+                              alpha, descr, 
+                              d_csrValA, d_csrRowPtrA, d_csrColIndA, 
+                              d_inputVector, beta, d_spmvResult );
+
+    switch( status ) {
+        case CUSPARSE_STATUS_SUCCESS:
+            //printf("spmv multiplication successful!\n");
+            break;
+        case CUSPARSE_STATUS_NOT_INITIALIZED:
+            printf("Error: Library not initialized.\n");
+            break;
+        case CUSPARSE_STATUS_INVALID_VALUE:
+            printf("Error: Invalid parameters m, n, or nnz.\n");
+            break;
+        case CUSPARSE_STATUS_EXECUTION_FAILED:
+            printf("Error: Failed to launch GPU.\n");
+            break;
+        case CUSPARSE_STATUS_ALLOC_FAILED:
+            printf("Error: Resources could not be allocated.\n");
+            break;
+        case CUSPARSE_STATUS_ARCH_MISMATCH:
+            printf("Error: Device architecture does not support.\n");
+            break;
+        case CUSPARSE_STATUS_INTERNAL_ERROR:
+            printf("Error: An internal operation failed.\n");
+            break;
+        case CUSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED:
+            printf("Error: Matrix type not supported.\n");
+    }
+
+    // Important: destroy handle
+    cusparseDestroy(handle);
+    cusparseDestroyMatDescr(descr);
+}
+
+__global__ void addResult( int *d_bfsResult, const float *d_spmvResult, const int iter ) {
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+
+    //while( tid<N ) {
+        d_bfsResult[tid] = (d_spmvResult[tid]>0.5 && d_bfsResult[tid]<0) ? iter : d_bfsResult[tid];
+    //    tid += blockDim.x*gridDim.x;
+    //}
+}
+
+void bfs( const int vertex, const int edge, const int m, const int *d_csrRowPtrA, const int *d_csrColIndA, int *d_bfsResult, const int depth ) {
+
+    // Allocate GPU memory for result
+    float *d_spmvResult, *d_spmvSwap;
+    cudaMalloc(&d_spmvResult, m*sizeof(float));
+    cudaMalloc(&d_spmvSwap, m*sizeof(float));
+
+    // Generate initial vector using vertex
+    int *h_bfsResult;
+    float *h_spmvResult;
+    h_bfsResult = (int*)malloc(m*sizeof(int));
+    h_spmvResult = (float*)malloc(m*sizeof(float));
+
+    for( int i=0; i<m; i++ ) {
+        h_bfsResult[i]=-1;
+        h_spmvResult[i]=0;
+        if( i==vertex ) {
+            h_bfsResult[i]=0;
+            h_spmvResult[i]=1;
+        }
+    }
+    //std::cout << "This is m: " << m << std::endl;
+    //print_array(h_bfsResult,m);
+    cudaMemcpy(d_spmvSwap,h_spmvResult, m*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_bfsResult,h_bfsResult, m*sizeof(int), cudaMemcpyHostToDevice);
+
+    // Generate values for BFS (csrValA where everything is 1)
+    float *h_bfsValA, *d_bfsValA;
+    h_bfsValA = (float*)malloc(edge*sizeof(float));
+    cudaMalloc(&d_bfsValA, edge*sizeof(float));
+
+    for( int i=0; i<edge; i++ ) {
+        h_bfsValA[i] = 1;
+    }
+    cudaMemcpy(d_bfsValA, h_bfsValA, edge*sizeof(float), cudaMemcpyHostToDevice);
+    
+    // Use Thrust to generate initial vector
+    //thrust::device_ptr<float> dev_ptr(d_inputVector);
+    //thrust::fill(dev_ptr, dev_ptr + m, (float) 0);
+    //thrust::device_vector<float> inputVector(m, 0);
+    //inputVector[vertex] = 1;
+    //d_inputVector = thrust::raw_pointer_cast( &inputVector[0] );
+
+    GpuTimer gpu_timer;
+    float elapsed = 0.0f;
+    gpu_timer.Start();
+    cudaProfilerStart();
+    spmv(d_spmvSwap, edge, m, d_bfsValA, d_csrRowPtrA, d_csrColIndA, d_spmvResult);
+    
+    int NBLOCKS = (m+NTHREADS-1)/NTHREADS;
+
+    //axpy(d_spmvSwap, d_bfsValA, m);
+    addResult<<<NBLOCKS,NTHREADS>>>( d_bfsResult, d_spmvResult, 1 );
+
+    for( int i=2; i<depth; i++ ) {
+    //for( int i=2; i<3; i++ ) {
+        if( i%2==0 ) {
+            spmv( d_spmvResult, edge, m, d_bfsValA, d_csrRowPtrA, d_csrColIndA, d_spmvSwap );
+            addResult<<<NBLOCKS,NTHREADS>>>( d_bfsResult, d_spmvSwap, i );
+        } else {
+            spmv( d_spmvSwap, edge, m, d_bfsValA, d_csrRowPtrA, d_csrColIndA, d_spmvResult );
+            addResult<<<NBLOCKS,NTHREADS>>>( d_bfsResult, d_spmvResult, i );
+        }
+    }
+
+    cudaProfilerStop();
+    gpu_timer.Stop();
+    elapsed += gpu_timer.ElapsedMillis();
+    printf("GPU BFS finished in %f msec. \n", elapsed);
+
+    //cudaMemcpy(h_spmvResult,d_spmvSwap, m*sizeof(float), cudaMemcpyDeviceToHost);
+    //cudaMemcpy(h_bfsResult,d_bfsResult, m*sizeof(int), cudaMemcpyDeviceToHost);
+    //print_array(h_spmvResult,m);
+    //print_array(h_bfsResult,m);
+
+    cudaFree(d_spmvResult);
+    cudaFree(d_spmvSwap);
+    free(h_bfsResult);
+    free(h_spmvResult);
+}
+
 int main(int argc, char**argv) {
     int m, n, edge;
 
@@ -203,7 +375,70 @@ int main(int argc, char**argv) {
     printf("Testing %s\n", argv[1]);
 
     // File i/o
-    bool weighted;
+    /*FILE *input = fopen(argv[1], "r");
+
+    bool directed;
+    int c = fgetc(input);
+    int old_c = 0;
+
+    //printf("%d\n",c);
+    while( c!=EOF ) {
+        if( (old_c==10 || old_c==0) && c!=37 ) {
+            ungetc(c, input);
+            //printf("%d %d\n",old_c,c);
+            break;
+        }
+        old_c = c;
+        c = fgetc(input);
+    }
+    fscanf(input, "%d %d %d", &m, &n, &edge);
+    
+    // Allocate memory depending on how many edges are present
+    float *h_csrValA;
+    int *h_csrRowPtrA, *h_csrColIndA, *h_cooRowIndA;
+    int *h_bfsResult, *h_bfsResultCPU;
+
+    h_csrValA    = (float*)malloc(edge*sizeof(float));
+    h_csrRowPtrA = (int*)malloc((m+1)*sizeof(int));
+    h_csrColIndA = (int*)malloc(edge*sizeof(int));
+    h_cooRowIndA = (int*)malloc(edge*sizeof(int));
+    h_bfsResult = (int*)malloc((m)*sizeof(int));
+    h_bfsResultCPU = (int*)malloc((m)*sizeof(int));
+
+    // Currently checks if there are fewer rows than promised
+    // Could add check for edges in diagonal of adjacency matrix
+    for( int j=0; j<edge; j++ ) {
+        if( fscanf(input, "%d", &h_csrColIndA[j])==EOF ) {
+            printf("Error: not enough rows in mtx file.\n");
+            break;
+        }
+        fscanf(input, "%d", &h_cooRowIndA[j]);
+
+        if( j==0 ) {
+            c=fgetc(input);
+            //printf("c = %d\n",c);
+        }
+
+        if( c!=32 ) {
+            h_csrValA[j]=1.0;
+            if( j==0 ) directed = false;
+        } else {
+            fscanf(input, "%f", &h_csrValA[j]);
+        }
+
+        h_cooRowIndA[j]--;
+        h_csrColIndA[j]--;
+        //printf("%d %d %d\n", h_cooRowIndA[j], h_csrColIndA[j], j);
+    }
+    fclose(input);
+    if( directed==true ) {
+        printf("The graph is directed: ");
+        print_end(h_csrValA,edge);
+    } else {
+        printf("The graph is undirected.\n");
+    }*/
+
+    bool directed;
     int c = getchar();
     int old_c = 0;
     //printf("%d\n",c);
@@ -246,7 +481,7 @@ int main(int argc, char**argv) {
 
         if( c!=32 ) {
             h_csrValA[j]=1.0;
-            if( j==0 ) weighted = false;
+            if( j==0 ) directed = false;
         } else {
             scanf("%f", &h_csrValA[j]);
         }
@@ -255,7 +490,7 @@ int main(int argc, char**argv) {
         h_csrColIndA[j]--;
         //printf("%d %d %d\n", h_cooRowIndA[j], h_csrColIndA[j], j);
     }
-    if( weighted==true ) {
+    if( directed==true ) {
         printf("The graph is weighted: ");
         print_end(h_csrValA,edge);
     } else {
@@ -268,7 +503,6 @@ int main(int argc, char**argv) {
     float *d_cscValA;
     int *d_cscRowIndA, *d_cscColPtrA;
     int *d_bfsResult;
-
     cudaMalloc(&d_bfsResult, m*sizeof(int));
 
     cudaMalloc(&d_csrValA, edge*sizeof(float));
@@ -281,9 +515,11 @@ int main(int argc, char**argv) {
     cudaMalloc(&d_cscColPtrA, (m+1)*sizeof(int));
 
     // Copy data from host to device
-    cudaMemcpy(d_csrValA, h_csrValA, edge*sizeof(int),cudaMemcpyHostToDevice);
-    cudaMemcpy(d_csrColIndA, h_csrColIndA, edge*sizeof(int),cudaMemcpyHostToDevice);
-    cudaMemcpy(d_cooRowIndA, h_cooRowIndA, edge*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_csrValA, h_csrValA, (edge)*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_csrColIndA, h_csrColIndA, (edge)*sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(d_cooRowIndA, h_cooRowIndA, (edge)*sizeof(int),cudaMemcpyHostToDevice);
+    //cudaMemcpy(h_cooRowIndA, d_cooRowIndA, (edge)*sizeof(int),cudaMemcpyDeviceToHost);
+    //print_vector(h_cooRowIndA,edge);
 
     // Run COO -> CSR kernel
     coo2csr( d_cooRowIndA, edge, m, d_csrRowPtrA );
@@ -299,41 +535,23 @@ int main(int argc, char**argv) {
     //int depth = 4;
     //bfsCPU( 0, m, h_csrRowPtrA, h_csrColIndA, h_bfsResultCPU, depth );
     //depth++;
-    //print_end_interesting(h_bfsResultCPU, m);
+    print_end_interesting(h_bfsResultCPU, m);
 
     GpuTimer gpu_timer;
     GpuTimer gpu_timer2;
     float elapsed = 0.0f;
     float elapsed2 = 0.0f;
     gpu_timer.Start();
-
     // Run CSR -> CSC kernel
     csr2csc( m, edge, d_csrValA, d_csrRowPtrA, d_csrColIndA, d_cscValA, d_cscRowIndA, d_cscColPtrA );
     gpu_timer2.Start();
 
-    cudaFree(d_csrValA);
-    cudaFree(d_csrRowPtrA);
-    cudaFree(d_csrColIndA);
-    cudaFree(d_cooRowIndA);
-    cudaFree(d_cscValA);
-
-    cudaMemcpy(h_csrRowPtrA, d_cscColPtrA, (m+1)*sizeof(int),cudaMemcpyDeviceToHost);
-    cudaMemcpy(h_csrColIndA, d_cscRowIndA, edge*sizeof(int),cudaMemcpyDeviceToHost);
-    cudaFree(d_cscColPtrA);
-    cudaFree(d_cscRowIndA);
-
-    // Allocate GPU memory for result
-    float *d_spmvResult, *d_spmvSwap;
-    cudaMalloc(&d_spmvResult, m*sizeof(float));
-    cudaMalloc(&d_spmvSwap, m*sizeof(float));
-
-    Pack pack;
-    pack.copyPack( 0, m, edge, depth, h_csrRowPtrA, h_csrColIndA );
-
     // Run BFS kernel on GPU
-    bfs( pack, d_bfsResult, d_spmvResult, d_spmvSwap );
-    pack.Free(); 
+    // Non-transpose spmv
+    //bfs( i, edge, m, d_csrValA, d_csrRowPtrA, d_csrColIndA, d_bfsResult, 5 );
+    //bfs( 0, edge, m, d_cscValA, d_cscColPtrA, d_cscRowIndA, d_bfsResult, 5 );
 
+    bfs( 0, edge, m, d_cscColPtrA, d_cscRowIndA, d_bfsResult, depth );
     gpu_timer.Stop();
     gpu_timer2.Stop();
     elapsed += gpu_timer.ElapsedMillis();
@@ -347,16 +565,20 @@ int main(int argc, char**argv) {
     verify( m, h_bfsResult, h_bfsResultCPU );
     print_array(h_bfsResult, m);
 
-    cudaFree(d_bfsResult);
+    cudaFree(d_csrValA);
+    cudaFree(d_csrRowPtrA);
+    cudaFree(d_csrColIndA);
+    cudaFree(d_cooRowIndA);
 
-    cudaFree(d_spmvResult);
-    cudaFree(d_spmvSwap);
+    cudaFree(d_cscValA);
+    cudaFree(d_cscRowIndA);
+    cudaFree(d_cscColPtrA);
+    cudaFree(d_bfsResult);
 
     free(h_csrValA);
     free(h_csrRowPtrA);
     free(h_csrColIndA);
     free(h_cooRowIndA);
-
     free(h_bfsResult);
     free(h_bfsResultCPU);
 
