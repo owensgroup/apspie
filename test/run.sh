259f638e9aacff4860eedb1bba696fe1f42989c0
Carl Yang <ctcyang@ucdavis.edu>
2015-01-06 14:08:59 -0800

    run.sh

--- test/run.sh
+++ test/run.sh
@@ -2,17 +2,17 @@
 
 ARCH="GEN_SM35"
 
-#if [ "$ARCH" = "GEN_SM20" ] ; then
-#    ./test ../dataset/small/test_cc.mtx
-#    ./test ../dataset/small/test_bc.mtx
-#    ./test ../dataset/small/test_pr.mtx
-#    ./test ../dataset/small/chesapeake.mtx
-#else
-#    ./test /data/gunrock_dataset/small/test_cc.mtx
-#    ./test /data/gunrock_dataset/small/test_bc.mtx
-#    ./test /data/gunrock_dataset/small/test_pr.mtx
-#    ./test /data/gunrock_dataset/small/chesapeake.mtx
-#fi
+if [ "$ARCH" = "GEN_SM20" ] ; then
+    ./test ../dataset/small/test_cc.mtx
+    ./test ../dataset/small/test_bc.mtx
+    ./test ../dataset/small/test_pr.mtx
+    ./test ../dataset/small/chesapeake.mtx
+else
+    ./test /data/gunrock_dataset/small/test_cc.mtx
+    ./test /data/gunrock_dataset/small/test_bc.mtx
+    ./test /data/gunrock_dataset/small/test_pr.mtx
+    ./test /data/gunrock_dataset/small/chesapeake.mtx
+fi
 
 for i in ak2010 belgium_osm coAuthorsDBLP delaunay_n13 delaunay_n21 webbase-1M soc-LiveJournal1 kron_g500-logn21
 do
