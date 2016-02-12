651a6a0b5471b76c2f5b0143a9ee0545664b38ef
Carl Yang <ctcyang@ucdavis.edu>
2016-02-11 22:04:45 -0800

    added allocScratch for allocating scratchpad memory

--- tests/tc/main.cu
+++ tests/tc/main.cu
@@ -16,7 +16,7 @@
 #include <moderngpu.cuh>
 
 #include <util.cuh>
-#include <bfs.cuh>
+#include <tc.cuh>
 //#include <spmspvMM.cuh>
 
 #include <testBfs.cpp>
@@ -36,7 +36,8 @@ void runBfs(int argc, char**argv) {
     int source;
     int device;
     float delta;
-    if( parseArgs( argc, argv, source, device, delta )==true ) {
+    bool undirected = false;
+    if( parseArgs( argc, argv, source, device, delta, undirected )==true ) {
         printf( "Usage: test apple.mtx -source 5\n");
         return;
     }
