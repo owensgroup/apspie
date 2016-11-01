d9edbf83e9cfe9d801d03ab4cf58c332c2cf3836
Carl Yang <ctcyang@ucdavis.edu>
2016-11-01 13:02:52 -0700

    IPDPS last check in

--- src/spgemm.cuh
+++ src/spgemm.cuh
@@ -15,7 +15,7 @@
 
 #define NTHREADS 512
 #define MAX_SHARED 49152
-#define MAX_GLOBAL 191000000
+#define MAX_GLOBAL 270000000
 
 template< typename typeVal >
 __global__ void populateRowIndVal( const int *d_cscColPtr, const int *d_cscRowInd, const typeVal *d_cscVal, int *d_dcscPartPtr, int *d_dcscColPtr_ind, int *d_dcscColPtr_off, int *d_dcscRowInd, typeVal *d_dcscVal, const int partNum, const int col_length, const int m, const int nnz )
@@ -181,7 +181,7 @@ void spgemm( d_matrix *C, d_matrix *A, d_matrix *B, const int partSize, const in
 
 	// Important mallocs
     // Step 0: Preallocations for scratchpad memory
-    int *d_flagArray, *d_tempArray, *d_lionArray, *d_sootArray, *d_index;
+    /*int *d_flagArray, *d_tempArray, *d_lionArray, *d_sootArray;
 	int *d_catmArray, *d_ouseArray, *d_rootArray, *d_beerArray;
 	float *d_tempValA, *d_tempValB;
     CUDA_SAFE_CALL(cudaMalloc(&d_flagArray, A->nnz*sizeof(int)));
@@ -191,11 +191,12 @@ void spgemm( d_matrix *C, d_matrix *A, d_matrix *B, const int partSize, const in
     CUDA_SAFE_CALL(cudaMalloc(&d_catmArray, A->nnz*sizeof(int)));
     CUDA_SAFE_CALL(cudaMalloc(&d_ouseArray, 10*A->nnz*sizeof(int)));
     CUDA_SAFE_CALL(cudaMalloc(&d_rootArray, A->nnz*sizeof(int)));
-    CUDA_SAFE_CALL(cudaMalloc(&d_beerArray, 10*A->nnz*sizeof(int)));
-    CUDA_SAFE_CALL(cudaMalloc(&d_tempValA, 10*A->nnz*sizeof(float)));
-    CUDA_SAFE_CALL(cudaMalloc(&d_tempValB, 10*A->nnz*sizeof(float)));
+    CUDA_SAFE_CALL(cudaMalloc(&d_beerArray, 70*A->nnz*sizeof(int)));
+    CUDA_SAFE_CALL(cudaMalloc(&d_tempValA, 70*A->nnz*sizeof(float)));
+    CUDA_SAFE_CALL(cudaMalloc(&d_tempValB, 10*A->nnz*sizeof(float)));*/
 
 	// d_index
+	int *d_index;
     CUDA_SAFE_CALL(cudaMalloc(&d_index, A->nnz*sizeof(int)));
     int *h_index = (int*)malloc(A->nnz*sizeof(int));
     for( int i=0; i<A->nnz;i++ ) h_index[i]=i;
@@ -210,7 +211,7 @@ void spgemm( d_matrix *C, d_matrix *A, d_matrix *B, const int partSize, const in
 	GpuTimer gpu_timer2;
 	gpu_timer2.Start();
 
-	for( int i=0; i<1; i++ ) {
+	/*for( int i=0; i<1; i++ ) {
 	//for( int i=0; i<partNum; i++ ) {
 
 	int size = h_inter[(i+1)*partNum]-h_inter[i*partNum];
@@ -274,9 +275,9 @@ void spgemm( d_matrix *C, d_matrix *A, d_matrix *B, const int partSize, const in
 		print_array_device( "B ColVal", d_tempValA, length);
 		print_array_device( "scanbalance", d_scanbalance, h_inter[(i+1)*partNum]);
 		}
-	}
+	}*/
 
-	//LaunchKernel<float>( C, A, B, d_intersectionA, d_intersectionB, d_interbalance, h_inter, max_AB, partSize, partNum, context );
+	LaunchKernel<float>( C, A, B, d_intersectionA, d_intersectionB, d_interbalance, h_inter, max_AB, partSize, partNum, context );
 	gpu_timer2.Stop();
 
 	elapsed2 += gpu_timer2.ElapsedMillis();
@@ -388,51 +389,6 @@ void spgemmInner( d_matrix *C, d_matrix *A, d_matrix *B, const int partSize, con
 	return BHSPARSE_SUCCESS;
 }*/
 
-// Uses cuSPARSE SpMV
-template<typename T>
-void cuspmv( const T *d_inputVector, const int edge, const int m, const T *d_csrValA, const int *d_csrRowPtrA, const int *d_csrColIndA, T *d_spmvResult, cusparseHandle_t handle, cusparseMatDescr_t descr ) {
-
-    const float alf = 1;
-    const float bet = 0;
-    const float *alpha = &alf;
-    const float *beta = &bet;
-
-    // For CUDA 5.0+
-    cusparseStatus_t status = cusparseScsrmv(handle,                   
-                              CUSPARSE_OPERATION_NON_TRANSPOSE, 
-                              m, m, edge, 
-                              alpha, descr, 
-                              d_csrValA, d_csrRowPtrA, d_csrColIndA, 
-                              d_inputVector, beta, d_spmvResult );
-
-    switch( status ) {
-        case CUSPARSE_STATUS_SUCCESS:
-            printf("spmv multiplication successful!\n");
-            break;
-        case CUSPARSE_STATUS_NOT_INITIALIZED:
-            printf("Error: Library not initialized.\n");
-            break;
-        case CUSPARSE_STATUS_INVALID_VALUE:
-            printf("Error: Invalid parameters m, n, or nnz.\n");
-            break;
-        case CUSPARSE_STATUS_EXECUTION_FAILED:
-            printf("Error: Failed to launch GPU.\n");
-            break;
-        case CUSPARSE_STATUS_ALLOC_FAILED:
-            printf("Error: Resources could not be allocated.\n");
-            break;
-        case CUSPARSE_STATUS_ARCH_MISMATCH:
-            printf("Error: Device architecture does not support.\n");
-            break;
-        case CUSPARSE_STATUS_INTERNAL_ERROR:
-            printf("Error: An internal operation failed.\n");
-            break;
-        case CUSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED:
-            printf("Error: Matrix type not supported.\n");
-    }
-
-}
-
 // Uses cuSPARSE SpGEMM
 template<typename T>
 void spgemm( d_matrix *C, d_matrix *A, d_matrix *B ) {
@@ -534,214 +490,4 @@ void spgemm( d_matrix *C, d_matrix *A, d_matrix *B ) {
     // Important: destroy handle
     cusparseDestroy(handle);
     cusparseDestroyMatDescr(descr);
-}
-
-// TODO: move TC code to here
-template< typename T >
-__global__ void ewiseMultTc( const int edge, const int m, const T *d_cscValD, const int *d_cscColPtrD, const int *d_cscRowIndD, const T *d_cscValC, const int *d_cscColPtrC, const int *d_cscRowIndC, T *d_cscVecVal ) {
-    int stride = gridDim.x * blockDim.x;
-    int tid = blockIdx.x*blockDim.x + threadIdx.x;
-    T count = 0;
-    int i, k;
-    int i_end, k_end;
-    int i_row, k_row;
-    for (int idx = tid; idx < m; idx += stride) {
-        i = d_cscColPtrC[idx];
-        k = d_cscColPtrD[idx];
-        i_end = d_cscColPtrC[idx+1];
-        k_end = d_cscColPtrD[idx+1];
-
-        i_row = d_cscRowIndC[i];
-        k_row = d_cscRowIndD[k];
-        while( i<i_end && k<k_end && k_row < idx ) {
-            int diff = i_row - k_row;
-            if( diff == 0 )
-                count += d_cscValD[k];
-            if( diff <= 0 ) {
-                i++;
-                i_row = d_cscRowIndC[i];
-            }
-            if( diff >= 0 ) {
-                k++;
-                k_row = d_cscRowIndD[k];
-            }
-        }
-    }
-    d_cscVecVal[tid] = count;
-}
-
-__global__ void addResult( int *d_bfsResult, float *d_spmvResult, const int iter, const int length ) {
-    const int STRIDE = gridDim.x * blockDim.x;
-    for (int idx = (blockIdx.x * blockDim.x) + threadIdx.x; idx < length; idx += STRIDE) {
-        //d_bfsResult[idx] = (d_spmvResult[idx]>0.5 && d_bfsResult[idx]<0) ? iter:d_bfsResult[idx];
-        if( d_spmvResult[idx]>0.5 && d_bfsResult[idx]<0 ) {
-            d_bfsResult[idx] = iter;
-        } else d_spmvResult[idx] = 0;
-    }
-    //int tid = threadIdx.x + blockIdx.x * blockDim.x;
-
-    //while( tid<N ) {
-        //d_bfsResult[tid] = (d_spmvResult[tid]>0.5 && d_bfsResult[tid]<0) ? iter : d_bfsResult[tid];
-    //    tid += blockDim.x*gridDim.x;
-    //}
-}
-
-template< typename T >
-void bfs( const int vertex, const int edge, const int m, const T* d_cscValA, const int *d_cscColPtrA, const int *d_cscRowIndA, int *d_bfsResult, const int depth, mgpu::CudaContext& context) {
-
-    cusparseHandle_t handle;
-    cusparseCreate(&handle);
-
-    cusparseMatDescr_t descr;
-    cusparseCreateMatDescr(&descr);
-
-    // Allocate scratch memory
-    d_scratch *d;
-    allocScratch( &d, edge, m );
-
-    // Allocate GPU memory for result
-    float *d_spmvResult, *d_spmvSwap;
-    cudaMalloc(&d_spmvResult, m*sizeof(float));
-    cudaMalloc(&d_spmvSwap, m*sizeof(float));
-
-    // Generate initial vector using vertex
-    // Generate d_ones, d_index
-    for( int i=0; i<m; i++ ) {
-        d->h_bfsResult[i]=-1;
-        d->h_spmvResult[i]=0.0;
-        d->h_ones[i] = 1;
-        d->h_index[i] = i;
-        if( i==vertex ) {
-            d->h_bfsResult[i]=0;
-            d->h_spmvResult[i]=1.0;
-        }
-    }
-    cudaMemcpy(d_bfsResult, d->h_bfsResult, m*sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_spmvSwap, d->h_spmvResult, m*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d->d_index, d->h_index, m*sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d->d_ones, d->h_ones, m*sizeof(int), cudaMemcpyHostToDevice);
-
-    // Generate d_cscColDiff
-    int NBLOCKS = (m+NTHREADS-1)/NTHREADS;
-    diff<<<NBLOCKS,NTHREADS>>>(d_cscColPtrA, d->d_cscColDiff, m);
-
-    // Generate values for BFS (cscValA where everything is 1)
-    float *d_bfsValA;
-    cudaMalloc(&d_bfsValA, edge*sizeof(float));
-
-    for( int i=0; i<edge; i++ ) {
-        d->h_bfsValA[i] = 1.0;
-    }
-    cudaMemcpy(d_bfsValA, d->h_bfsValA, edge*sizeof(float), cudaMemcpyHostToDevice);
-
-    GpuTimer gpu_timer;
-    float elapsed = 0.0f;
-    gpu_timer.Start();
-    cudaProfilerStart();
-
-    //spmv<float>(d_spmvSwap, edge, m, d_bfsValA, d_cscColPtrA, d_cscRowIndA, d_spmvResult, context);
-    //cuspmv<float>(d_spmvSwap, edge, m, d_bfsValA, d_cscColPtrA, d_cscRowIndA, d_spmvResult, handle, descr);
-    mXv<float>(d_spmvSwap, edge, m, d_bfsValA, d_cscColPtrA, d_cscRowIndA, d_spmvResult, d, context);
-
-    addResult<<<NBLOCKS,NTHREADS>>>( d_bfsResult, d_spmvResult, 1, m);
-
-    for( int i=2; i<depth; i++ ) {
-    //for( int i=2; i<5; i++ ) {
-        if( i%2==0 ) {
-            //spmv<float>( d_spmvResult, edge, m, d_bfsValA, d_cscColPtrA, d_cscRowIndA, d_spmvSwap, context);
-            //cuspmv<float>( d_spmvResult, edge, m, d_bfsValA, d_cscColPtrA, d_cscRowIndA, d_spmvSwap, handle, descr);
-            mXv<float>( d_spmvResult, edge, m, d_bfsValA, d_cscColPtrA, d_cscRowIndA, d_spmvSwap, d, context);
-            addResult<<<NBLOCKS,NTHREADS>>>( d_bfsResult, d_spmvSwap, i, m);
-            //cudaMemcpy(h_bfsResult,d_bfsResult, m*sizeof(int), cudaMemcpyDeviceToHost);
-            //print_array(h_bfsResult,m);
-            //cudaMemcpy(h_spmvResult,d_spmvSwap, m*sizeof(float), cudaMemcpyDeviceToHost);
-            //print_array(h_spmvResult,m);
-        } else {
-            //spmv<float>( d_spmvSwap, edge, m, d_bfsValA, d_cscColPtrA, d_cscRowIndA, d_spmvResult, context);
-            //cuspmv<float>( d_spmvSwap, edge, m, d_bfsValA, d_cscColPtrA, d_cscRowIndA, d_spmvResult, handle, descr);
-            mXv<float>( d_spmvSwap, edge, m, d_bfsValA, d_cscColPtrA, d_cscRowIndA, d_spmvResult, d, context);
-            addResult<<<NBLOCKS,NTHREADS>>>( d_bfsResult, d_spmvResult, i, m);
-            //cudaMemcpy(h_bfsResult,d_bfsResult, m*sizeof(int), cudaMemcpyDeviceToHost);
-            //print_array(h_bfsResult,m);
-            //cudaMemcpy(h_spmvResult,d_spmvResult, m*sizeof(float), cudaMemcpyDeviceToHost);
-            //print_array(h_spmvResult,m);
-        }
-    }
-
-    cudaProfilerStop();
-    gpu_timer.Stop();
-    elapsed += gpu_timer.ElapsedMillis();
-    printf("\nGPU BFS finished in %f msec. \n", elapsed);
-    //printf("The maximum frontier size was: %d.\n", frontier_max);
-    //printf("The average frontier size was: %d.\n", frontier_sum/depth);
-
-    // Important: destroy handle
-    cusparseDestroy(handle);
-    cusparseDestroyMatDescr(descr);
-
-    cudaFree(d_spmvResult);
-    cudaFree(d_spmvSwap);
-}
-
-template< typename T >
-int mXm( const int edge, const int m, const T* d_cscValA, const int *d_cscColPtrA, const int *d_cscRowIndA, const T* d_cscValB, const int *h_cscColPtrB, const int *d_cscColPtrB, const int *d_cscRowIndB, T *d_cscValC, int *d_cscColPtrC, int *d_cscRowIndC, mgpu::CudaContext& context) {
-
-    // Allocate scratch memory
-    d_scratch *d;
-    allocScratch( &d, edge, m );
-
-    // Generate d_ones, d_index
-    for( int i=0; i<m; i++ ) {
-        d->h_ones[i] = 1;
-        d->h_index[i] = i;
-    }
-    cudaMemcpy(d->d_index, d->h_index, m*sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d->d_ones, d->h_ones, m*sizeof(int), cudaMemcpyHostToDevice);
-    cudaMalloc((void**) &d_cscColPtrC, (m+1)*sizeof(int));
-    cudaMalloc((void**) &d_cscRowIndC, 16*edge*sizeof(int));
-    cudaMalloc((void**) &d_cscValC, 16*edge*sizeof(T));
-
-    // Generate d_cscColDiff
-    int NBLOCKS = (m+NTHREADS-1)/NTHREADS;
-    diff<<<NBLOCKS,NTHREADS>>>(d_cscColPtrA, d->d_cscColDiff, m);
-
-    // Initialize nnz cumulative
-    int *h_cscColPtrC = (int*)malloc((m+1)*sizeof(int));
-    int total_nnz = 0;
-    h_cscColPtrC[0] = total_nnz;
-    int nnz = 0;
-
-    GpuTimer gpu_timer;
-    float elapsed = 0.0f;
-    gpu_timer.Start();
-    cudaProfilerStart();
-
-    for( int i=0; i<m; i++ ) {
-    //for( int i=0; i<2; i++ ) {
-        nnz = h_cscColPtrB[i+1]-h_cscColPtrB[i];
-        //printf("Reading %d elements in matrix B: %d to %d\n", nnz, h_cscColPtrB[i], h_cscColPtrB[i+1]);
-        if( nnz ) {
-        mXv<T>(&d_cscRowIndB[h_cscColPtrB[i]], &d_cscValB[h_cscColPtrB[i]], edge, m, nnz, d_cscValA, d_cscColPtrA, d_cscRowIndA, &d_cscRowIndC[total_nnz], &d_cscValC[total_nnz], d, context);
-        total_nnz += nnz;
-        h_cscColPtrC[i+1] = total_nnz;
-        //printf("mXv iteration %d: ColPtrC at %d\n", i, total_nnz);
-        /*cudaMemcpy(d->h_bfsResult, d_cscRowIndC, total_nnz*sizeof(int), cudaMemcpyDeviceToHost);
-        print_array(d->h_bfsResult,total_nnz);
-        cudaMemcpy(d->h_spmvResult, d_cscValC, total_nnz*sizeof(float), cudaMemcpyDeviceToHost);
-        print_array(d->h_spmvResult,total_nnz);*/
-        }
-    }
-
-    cudaProfilerStop();
-    gpu_timer.Stop();
-    elapsed += gpu_timer.ElapsedMillis();
-    printf("\nGPU mXm finished in %f msec. \n", elapsed);
-
-    int *h_cscRowIndC = (int*)malloc(total_nnz*sizeof(int));
-    T *h_cscValC = (T*)malloc(total_nnz*sizeof(T));
-    cudaMemcpy(h_cscRowIndC, d_cscRowIndC, total_nnz*sizeof(int), cudaMemcpyDeviceToHost);
-    cudaMemcpy(h_cscValC, d_cscValC, total_nnz*sizeof(T), cudaMemcpyDeviceToHost);
-    print_matrix(h_cscValC, h_cscColPtrC, h_cscRowIndC, m);
-
-    return total_nnz;
 }
